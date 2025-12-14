c5854aafa9f7f4ed60b50b0a6e064dc68af8fa7d
Audrey Zebaze <audrey.sobgouzebaze@gmail.com>
2025-12-14 14:36:49 +0100

    Initial Commit

---
+++ AFCON2025Tests/AFCON2025Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AFCON2025Tests.swift
+//  AFCON2025Tests
+//
+//  Created by Audrey Zebaze on 14/12/2025.
+//
+
+import Testing
+@testable import AFCON2025
+
+struct AFCON2025Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
